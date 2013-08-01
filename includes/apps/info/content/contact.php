94092420b9dbb098caf77cbcaef53362f27d8d65
Vince Pale <vince@panicgripdesigns.com>
2013-08-01 15:03:04 +0000

    Replaces contact form table with bootstrap form layout

--- includes/apps/info/content/contact.php
+++ includes/apps/info/content/contact.php
@@ -15,24 +15,28 @@
   }
 ?>
 
-<?php echo osc_draw_form('contact_us', osc_href_link('info', 'contact&process'), 'post', '', true); ?>
+<?php echo osc_draw_form('contact_us', osc_href_link('info', 'contact&process'), 'post', 'class="form-horizontal"', true); ?>
 
 <div class="contentContainer">
   <div class="contentText">
-    <table border="0" width="100%" cellspacing="0" cellpadding="2">
-      <tr>
-        <td class="fieldKey"><?php echo ENTRY_NAME; ?></td>
-        <td class="fieldValue"><?php echo osc_draw_input_field('name'); ?></td>
-      </tr>
-      <tr>
-        <td class="fieldKey"><?php echo ENTRY_EMAIL; ?></td>
-        <td class="fieldValue"><?php echo osc_draw_input_field('email'); ?></td>
-      </tr>
-      <tr>
-        <td class="fieldKey" valign="top"><?php echo ENTRY_ENQUIRY; ?></td>
-        <td class="fieldValue"><?php echo osc_draw_textarea_field('enquiry', 'soft', 50, 15); ?></td>
-      </tr>
-    </table>
+      <div class="control-group">
+          <label class="control-label" for="name"><?php echo ENTRY_NAME; ?></label>
+          <div class="controls">
+              <?php echo osc_draw_input_field('name', null, 'id="name" placeholder="'.ENTRY_NAME.'"' ); ?>
+          </div>
+      </div>
+      <div class="control-group">
+          <label class="control-label" for="email"><?php echo ENTRY_EMAIL; ?></label>
+          <div class="controls">
+              <?php echo osc_draw_input_field('email', null, 'id="email" placeholder="'.ENTRY_EMAIL.'"' ); ?>
+          </div>
+      </div>
+      <div class="control-group">
+          <label class="control-label" for="email"><?php echo ENTRY_ENQUIRY; ?></label>
+          <div class="controls">
+              <?php echo osc_draw_textarea_field('enquiry', 'soft', 50, 15); ?>
+          </div>
+      </div>
   </div>
 
   <div class="buttonSet">
